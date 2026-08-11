6219b2537ddc5760f4d223003e8fb8de094c3587
hmurtadh <hawoormurtadha@gmail.com>
2026-08-11 14:27:56 +0300

    h

--- js/profile.js
+++ js/profile.js
@@ -114,9 +114,9 @@ var failedPaths = {};
 
 projects.forEach(function (p) {
   if (!p.path) return;
-  if (p.grade > 0) {
+  if (p.grade >= 1) {
     passedPaths[p.path] = true;
-  } else if (p.grade === 0) {
+  } else if (p.grade < 1) {
     failedPaths[p.path] = true;
   }
 });
